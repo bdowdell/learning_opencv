c47cf70ac77764d3b92f8a89f1b1fd618f2c7da9
Ben Dowdell <ben.dowdell87@gmail.com>
2020-05-13 23:14:30 -0500

    updates load_image.cpp

--- src/load_image.cpp
+++ src/load_image.cpp
@@ -1,9 +1,26 @@
 #include <opencv2/highgui.hpp>
+#include <iostream>
 
 using namespace cv;
+using namespace std;
 
 int main( int argc, char** argv ) {
     //Read in an image from the command line
     Mat img = imread( argv[1] );
+
+    //Check for failure
+    if (img.empty())
+    {
+        cout << "Could not open or find the image" << endl;
+        cin.get();
+        return -1;
+    }
+
+    String windowName = "Viewer";
+    namedWindow(windowName);
+    imshow(windowName, img);
+    waitKey(0);
+    destroyWindow(windowName);
+
     return 0;
 }
