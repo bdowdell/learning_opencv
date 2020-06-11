8be85d8994cb6d792a4fac1304409315d986f498
Ben Dowdell <ben.dowdell87@gmail.com>
2020-06-10 22:09:11 -0500

    adds comments

--- src/xrs_5_2.cpp
+++ src/xrs_5_2.cpp
@@ -21,21 +21,21 @@ int main(){
     }*/
     // top & bottom sides
     for(int j = 5; j < 21; j++){
-        mat.at<cv::Vec3b>(20,j)[0] = 0;
-        mat.at<cv::Vec3b>(20,j)[1] = 255;
-        mat.at<cv::Vec3b>(20,j)[2] = 0;
-        mat.at<cv::Vec3b>(40,j)[0] = 0;
-        mat.at<cv::Vec3b>(40,j)[1] = 255;
-        mat.at<cv::Vec3b>(40,j)[2] = 0;
+        mat.at<cv::Vec3b>(20,j)[0] = 0; // blue channel top side
+        mat.at<cv::Vec3b>(20,j)[1] = 255; // green channel top side
+        mat.at<cv::Vec3b>(20,j)[2] = 0; // red channel top side
+        mat.at<cv::Vec3b>(40,j)[0] = 0; // blue channel bottom side
+        mat.at<cv::Vec3b>(40,j)[1] = 255; // green channel bottom side
+        mat.at<cv::Vec3b>(40,j)[2] = 0; // red channel bottom side
     }
     // left and right sides
     for(int i = 20; i < 41; i++){
-        mat.at<cv::Vec3b>(i,5)[0] = 0;
-        mat.at<cv::Vec3b>(i,20)[0] = 0;
-        mat.at<cv::Vec3b>(i,5)[1] = 255;
-        mat.at<cv::Vec3b>(i,20)[1] = 255;
-        mat.at<cv::Vec3b>(i,5)[2] = 0;
-        mat.at<cv::Vec3b>(i,20)[2] = 0;
+        mat.at<cv::Vec3b>(i,5)[0] = 0; // blue channel left side
+        mat.at<cv::Vec3b>(i,20)[0] = 0; // blue channel right side
+        mat.at<cv::Vec3b>(i,5)[1] = 255; // green channel left side
+        mat.at<cv::Vec3b>(i,20)[1] = 255; // green channel right side
+        mat.at<cv::Vec3b>(i,5)[2] = 0; // red channel left side
+        mat.at<cv::Vec3b>(i,20)[2] = 0; // red channel right side
     }
     string window_square = "Square";
     cv::namedWindow(window_square, cv::WINDOW_AUTOSIZE);
