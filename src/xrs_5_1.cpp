7f183dbfec3c55c1edfc4984e9c6a689c997f02a
Ben Dowdell <ben.dowdell87@gmail.com>
2020-06-10 21:38:29 -0500

    updates to red circle

--- src/xrs_5_1.cpp
+++ src/xrs_5_1.cpp
@@ -14,7 +14,7 @@ using namespace std;
 int main() {
     // create a 100x100 3-channel matrix with all values set to 0
     cv::Mat mat = cv::Mat(100, 100, CV_8UC3, 0.0);
-    cv::circle(mat, cv::Point2f(mat.cols/2, mat.rows/2), mat.cols/2, 255, 1, 8, 0);
+    cv::circle(mat, cv::Point2f(mat.cols/2, mat.rows/2), mat.cols/2, cv::Scalar(0,0,255), 1, 8, 0); // draws a red circle
     string window_circle = "Circle";
     cv::namedWindow(window_circle, cv::WINDOW_AUTOSIZE);
     cv::moveWindow(window_circle, 500, 300);
