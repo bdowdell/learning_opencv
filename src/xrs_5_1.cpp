e00ba892ca49bde6fa2bb27fad8b33de7fb35ce0
Ben Dowdell <ben.dowdell87@gmail.com>
2020-06-10 21:36:29 -0500

    adds xrs_5_1.cpp

---
+++ src/xrs_5_1.cpp
@@ -0,0 +1,25 @@
+/*
+This exercise will accustom you to the idea of many functions taking matrix types.
+Create a two-dimensional matrix with three channels of type byte with data size 100x100.
+Set all the values to 0.
+a) Draw a cirlce in the matrix
+b) Display the image
+*/
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    // create a 100x100 3-channel matrix with all values set to 0
+    cv::Mat mat = cv::Mat(100, 100, CV_8UC3, 0.0);
+    cv::circle(mat, cv::Point2f(mat.cols/2, mat.rows/2), mat.cols/2, 255, 1, 8, 0);
+    string window_circle = "Circle";
+    cv::namedWindow(window_circle, cv::WINDOW_AUTOSIZE);
+    cv::moveWindow(window_circle, 500, 300);
+    cv::imshow(window_circle, mat);
+    cout << "Press <esc> key to exit ... \n";
+    cv::waitKey(0);
+    return 0;
+}
